e8f7165239da989f43a5c6586152c2042c2672de
akram <mathakiyaakram@gmail.com>
2024-10-30 09:53:14 +0530

    typo fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,18 +1,20 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
 import cookieparser from "cookie-parser";
 
 const app = express();
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
-    Credential: true,
+    origin: "http://localhost:5173", // Frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Ensure credentials is set correctly
   })
 );
 
-app.use(express.json({limit: "20kb"}))
-app.use(express.urlencoded({extends: true, limit: "20kb"}))
-app.use(express.static("public"))
-app.use(cookieparser())
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieparser());
 
-export { app }
+export { app };
